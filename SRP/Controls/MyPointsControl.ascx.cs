2f0285b439522686a18571c68ab9742126db62e6
Harald Nagel <haraldnagel@mcldaz.org>
2015-11-20 15:56:09 -0700

    Fix my points to display properly on postback

--- SRP/Controls/MyPointsControl.ascx.cs
+++ SRP/Controls/MyPointsControl.ascx.cs
@@ -8,20 +8,18 @@ using System.Web.UI.WebControls;
 using GRA.SRP.DAL;
 using GRA.SRP.Utilities.CoreClasses;
 
-namespace GRA.SRP.Controls
-{
-    public partial class MyPointsControl : System.Web.UI.UserControl
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            if (Session["Patron"] == null) Response.Redirect("/");
+namespace GRA.SRP.Controls {
+    public partial class MyPointsControl : System.Web.UI.UserControl {
+        protected void Page_PreRender(object sender, EventArgs e) {
+            if(Session["Patron"] == null) {
+                Response.Redirect("/");
+            }
 
             LoadData();
         }
 
-        public void LoadData()
-        {
-            
+        public void LoadData() {
+
             var patron = (Patron)Session["Patron"];
             Session["ActHistPID"] = patron.PID;
 
@@ -29,17 +27,14 @@ namespace GRA.SRP.Controls
             lblPoints.Text = tp.ToInt();
 
             var pgm = Programs.FetchObject(patron.ProgID);
-            if (pgm.ProgramGameID > 0)
-            {
-                if (ProgramGame.FetchObject(pgm.ProgramGameID) != null)
-                {
+            if(pgm.ProgramGameID > 0) {
+                if(ProgramGame.FetchObject(pgm.ProgramGameID) != null) {
                     LoadNextLevelInfo(patron, pgm, tp);
                 }
             }
         }
 
-        public void LoadNextLevelInfo(Patron p, Programs pg, int tp)
-        {
+        public void LoadNextLevelInfo(Patron p, Programs pg, int tp) {
             int level, points;
             bool bonus;
             GetGameInfo(p, pg, tp, out level, out points, out bonus);
@@ -52,14 +47,12 @@ namespace GRA.SRP.Controls
 
         }
 
-        public void GetGameInfo(Patron patron, Programs pgm, int StartingPoints, out int level, out int points, out bool bonus)
-        {
+        public void GetGameInfo(Patron patron, Programs pgm, int StartingPoints, out int level, out int points, out bool bonus) {
             //Tally up the points  
             level = 0;
             points = 0;
             bonus = false;
-            if (pgm.ProgramGameID > 0)
-            {
+            if(pgm.ProgramGameID > 0) {
                 // only if we have a game we can earn badges by reading ....
                 var gm = ProgramGame.FetchObject(pgm.ProgramGameID);
                 var ds = ProgramGameLevel.GetAll(gm.PGID);
@@ -67,29 +60,26 @@ namespace GRA.SRP.Controls
                 var normalLevelTotalPoints = GetGameCompletionPoints(ds);
                 var bonusLevelTotalPoints = GetGameCompletionBonusPoints(ds, gm.BonusLevelPointMultiplier);
 
-                bonus = (StartingPoints > normalLevelTotalPoints);              
+                bonus = (StartingPoints > normalLevelTotalPoints);
 
                 // loop thru the levels to see where we are at ... before awarding the new points
                 var rp = StartingPoints;   //remaining points
-                if (bonus)
-                {
+                if(bonus) {
                     // if we are on the bonus, eliminate the "fully completed boards/levels) and then see what the remainder of the points is.
                     rp = rp - normalLevelTotalPoints;
                     level = ds.Tables[0].Rows.Count + 1;   // completed all the levels for the "normal"
 
-                    level = level + (int) ((int) rp/(int) bonusLevelTotalPoints)*(ds.Tables[0].Rows.Count + 1);
+                    level = level + (int)((int)rp / (int)bonusLevelTotalPoints) * (ds.Tables[0].Rows.Count + 1);
 
                     rp = rp % bonusLevelTotalPoints;
 
                 }
 
-                for (var i = 0; i < ds.Tables[0].Rows.Count; i++)
-                {
+                for(var i = 0; i < ds.Tables[0].Rows.Count; i++) {
                     var multiplier = (bonus ? gm.BonusLevelPointMultiplier : 1.00m);
                     var levelPoints = Convert.ToInt32(Convert.ToInt32(ds.Tables[0].Rows[i]["PointNumber"]) * multiplier);
                     rp = rp - levelPoints;
-                    if (rp < 0)
-                    {
+                    if(rp < 0) {
                         points = -rp;
                         break;
                     }
@@ -98,21 +88,17 @@ namespace GRA.SRP.Controls
             }
         }
 
-        public int GetGameCompletionPoints(DataSet ds)
-        {
+        public int GetGameCompletionPoints(DataSet ds) {
             var ret = 0;
-            for (var i = 0; i < ds.Tables[0].Rows.Count; i++)
-            {
+            for(var i = 0; i < ds.Tables[0].Rows.Count; i++) {
                 ret = ret + Convert.ToInt32(Convert.ToInt32(ds.Tables[0].Rows[i]["PointNumber"]));
             }
             return ret;
         }
 
-        public int GetGameCompletionBonusPoints(DataSet ds, decimal bonusLevelPointMultiplier)
-        {
+        public int GetGameCompletionBonusPoints(DataSet ds, decimal bonusLevelPointMultiplier) {
             var ret = 0;
-            for (var i = 0; i < ds.Tables[0].Rows.Count; i++)
-            {
+            for(var i = 0; i < ds.Tables[0].Rows.Count; i++) {
                 var multiplier = bonusLevelPointMultiplier;
                 var levelPoints = Convert.ToInt32(Convert.ToInt32(ds.Tables[0].Rows[i]["PointNumber"]) * multiplier);
                 ret = ret + levelPoints;
