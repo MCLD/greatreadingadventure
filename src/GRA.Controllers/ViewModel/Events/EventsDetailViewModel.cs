472481edef9657bc86bbc5a0874bf7160bf99fd5
Harald Nagel <hpn@haraldnagel.com>
2018-04-23 15:24:41 -0700

    Add metadata for events
    - Add metadata description tag for events
    - Add structured data for events

--- src/GRA.Controllers/ViewModel/Events/EventsDetailViewModel.cs
+++ src/GRA.Controllers/ViewModel/Events/EventsDetailViewModel.cs
@@ -9,5 +9,8 @@ namespace GRA.Controllers.ViewModel.Events
     {
         public GRA.Domain.Model.Event Event { get; set; }
         public string ProgramString { get; set; }
+        public bool ShowStructuredData { get; set; }
+        public string EventStart { get; set; }
+        public string EventEnd { get; set; }
     }
 }
