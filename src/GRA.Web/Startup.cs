edbb6f3674e122dca6316f894792ccdd3a84d365
Dan <danielwilcox@mcldaz.org>
2018-04-25 14:48:40 -0700

    Add 404 error handling

--- src/GRA.Web/Startup.cs
+++ src/GRA.Web/Startup.cs
@@ -356,7 +356,7 @@ namespace GRA.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseStatusCodePagesWithReExecute("/Error/Index", "?statusCode={0}");
             }
 
             app.ApplicationServices.GetService<Data.Context>().Migrate();
