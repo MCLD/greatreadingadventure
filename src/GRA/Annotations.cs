990131d64d8cc41939b3d870db73a04e17281e94
Harald Nagel <hpn@haraldnagel.com>
2019-03-12 14:00:33 -0700

    Move school names to constants, sort constants

--- src/GRA/Annotations.cs
+++ src/GRA/Annotations.cs
@@ -5,9 +5,9 @@
         public static class Info
         {
             public const string LetYouKnowWhen = "Thanks! We'll let you know when you can join the program.";
-            public const string YouCanJoinOn = "You can join {0} on {1}.";
-            public const string RegistrationNotOpenYet = "Registration for {0} is not open yet.";
             public const string ProgramEnded = "{0} has ended, please join us next time!";
+            public const string RegistrationNotOpenYet = "Registration for {0} is not open yet.";
+            public const string YouCanJoinOn = "You can join {0} on {1}.";
         }
 
         public static class Required
@@ -18,21 +18,26 @@
             public const string ProgramSelection = "Please select a program.";
             public const string Selection = "Please select a value for {0}.";
             public const string System = "Please select a system.";
-
         }
 
         public static class Validate
         {
+            public const string BookTitle = "When providing an author of a book, please also provide a title.";
             public const string Branch = "The branch you've selected is not valid, please select another branch.";
+            public const string CouldNotLog = "Sorry, we couldn't log your activity: {0}";
+            public const string Email = "The {0} field is not a valid e-mail address.";
             public const string EmailConfigured = "Username '{0}' does not have an email address configured.";
             public const string EmailSubscription = "Please let us know if you would like to receive emails throughout the program.";
             public const string FirstTime = "Please let us know if this is your first time participating in the program.";
+            public const string MaxLength = "The field {0} must be a string or array type with a maximum length of '{1}'.";
+            public const string MinLength = "The field {0} must be a string or array type with a minimum length of '{1}'.";
             public const string NotOpen = "The program is not accepting registrations at this time.";
             public const string NotOpenActivity = "The program is not open for activity at this time.";
             public const string NotOpenSignins = "The program is not accepting sign-ins at this time.";
             public const string Password = "The provided password is incorrect.";
             public const string PasswordsMatch = "Password and password confirmation do not match.";
             public const string Permission = "Permission denied.";
+            public const string Phone = "The {0} field is not a valid phone number.";
             public const string Program = "The program you've selected is not valid, please select another program.";
             public const string School = "The school you've selected is not valid, please select another school.";
             public const string System = "The system you've selected is not valid, please select another system.";
@@ -41,12 +46,6 @@
             public const string Username = "Could not find username '{0}'.";
             public const string UsernameTaken = "Someone has already chosen that username.";
             public const string WholeNumber = "Please enter a whole number greater than 0.";
-            public const string BookTitle = "When providing an author of a book, please also provide a title.";
-            public const string CouldNotLog = "Sorry, we couldn't log your activity: {0}";
-            public const string MaxLength = "The field {0} must be a string or array type with a maximum length of '{1}'.";
-            public const string MinLength = "The field {0} must be a string or array type with a minimum length of '{1}'.";
-            public const string Email = "The {0} field is not a valid e-mail address.";
-            public const string Phone = "The {0} field is not a valid phone number.";
         }
     }
 }
--- src/GRA/DisplayNames.cs
+++ src/GRA/DisplayNames.cs
@@ -3,23 +3,25 @@
     public static class DisplayNames
     {
         public const string Age = "Age";
-        public const string Title = "Title";
         public const string Author = "Author";
-        public const string SecretCode = "Secret Code";
-        public const string Username = "Username";
-        public const string Password = "Password";
-        public const string ConfirmPassword = "ConfirmPassword";
-        public const string FirstName = "First Name";
-        public const string LastName = "Last Name";
-        public const string ZipCode = "Zip Code";
-        public const string System = "System";
         public const string Branch = "Branch";
-        public const string Program = "Program";
-        public const string EmailAddress = "Email Address";
-        public const string PhoneNumber = "Phone Number";
-        public const string IsFirstTime = "Is this your first time participating?";
+        public const string ConfirmPassword = "ConfirmPassword";
         public const string DailyPersonalGoal = "Set a personal goal";
+        public const string EmailAddress = "Email Address";
+        public const string FirstName = "First Name";
+        public const string Homeschooled = "Homeschooled";
+        public const string IsFirstTime = "Is this your first time participating?";
+        public const string LastName = "Last Name";
+        public const string Password = "Password";
+        public const string PhoneNumber = "Phone Number";
+        public const string Program = "Program";
         public const string School = "School";
         public const string SchoolDistrict = "School District";
+        public const string SchoolNotListed = "School not listed";
+        public const string SecretCode = "Secret Code";
+        public const string System = "System";
+        public const string Title = "Title";
+        public const string Username = "Username";
+        public const string ZipCode = "Zip Code";
     }
 }
