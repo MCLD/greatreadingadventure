313bb971a734a916b1a2b724c0ea4881516ab3fa
Harald Nagel <hpn@haraldnagel.com>
2019-03-14 10:14:15 -0700

    Add data migration

---
+++ src/GRA.Data.SQLite/Migrations/20190314171312_add-prize-redeemer-info.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GRA.Data.SQLite.Migrations
+{
+    public partial class addprizeredeemerinfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TriggerList",
+                table: "ReportCriteria",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RedeemedByBranch",
+                table: "PrizeWinners",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RedeemedBySystem",
+                table: "PrizeWinners",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TriggerList",
+                table: "ReportCriteria");
+
+            migrationBuilder.DropColumn(
+                name: "RedeemedByBranch",
+                table: "PrizeWinners");
+
+            migrationBuilder.DropColumn(
+                name: "RedeemedBySystem",
+                table: "PrizeWinners");
+        }
+    }
+}
