96216f66c02825233d5d599322f08a95954d3766
Harald Nagel <hpn@haraldnagel.com>
2019-05-02 12:41:38 -0700

    Fix rebase issue; add carousel parent class

--- src/GRA.Controllers/ViewModel/SignIn/PasswordRecoveryViewModel.cs
+++ src/GRA.Controllers/ViewModel/SignIn/PasswordRecoveryViewModel.cs
@@ -1,23 +1,24 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using GRA.Domain.Model;
 
 namespace GRA.Controllers.ViewModel.SignIn
 {
     public class PasswordRecoveryViewModel
     {
-        [Required(ErrorMessage = Annotations.Required.Field)]
+        [Required(ErrorMessage = ErrorMessages.Field)]
         [DisplayName(DisplayNames.Username)]
         public string Username { get; set; }
 
-        [Required(ErrorMessage = Annotations.Required.Field)]
+        [Required(ErrorMessage = ErrorMessages.Field)]
         [DisplayName(DisplayNames.Token)]
         public string Token { get; set; }
 
-        [Required(ErrorMessage = Annotations.Required.Field)]
+        [Required(ErrorMessage = ErrorMessages.Field)]
         [DisplayName(DisplayNames.NewPassword)]
         public string NewPassword { get; set; }
 
-        [Required(ErrorMessage = Annotations.Required.Field)]
+        [Required(ErrorMessage = ErrorMessages.Field)]
         [Compare(nameof(NewPassword), ErrorMessage = Annotations.Validate.PasswordsMatch)]
         [DisplayName(DisplayNames.ConfirmNewPassword)]
         public string ConfirmPassword { get; set; }
