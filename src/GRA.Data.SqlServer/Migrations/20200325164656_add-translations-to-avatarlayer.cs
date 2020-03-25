5f2a4b8707bed94780b772ffc6d105f39fd37b2d
madison-boman <maddieboman@gmail.com>
2020-03-25 09:56:26 -0700

    Add migration

---
+++ src/GRA.Data.SqlServer/Migrations/20200325164656_add-translations-to-avatarlayer.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GRA.Data.SqlServer.Migrations
+{
+    public partial class addtranslationstoavatarlayer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RemoveLabel",
+                table: "AvatarLayers",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SpanishName",
+                table: "AvatarLayers",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SpanishRemoveLabel",
+                table: "AvatarLayers",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RemoveLabel",
+                table: "AvatarLayers");
+
+            migrationBuilder.DropColumn(
+                name: "SpanishName",
+                table: "AvatarLayers");
+
+            migrationBuilder.DropColumn(
+                name: "SpanishRemoveLabel",
+                table: "AvatarLayers");
+        }
+    }
+}
