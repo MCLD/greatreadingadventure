7a8893c58e17f046c8d81e827df29ed7b3d78ef7
madison-boman <madi-boman@mcldaz.org>
2020-04-13 13:58:10 -0700

    Adding new badges logic for alt-text

--- src/GRA.Domain.Service/BadgeService.cs
+++ src/GRA.Domain.Service/BadgeService.cs
@@ -58,6 +58,7 @@ namespace GRA.Domain.Service
         public async Task<Badge> AddBadgeAsync(Badge badge, byte[] imageFile)
         {
             badge.SiteId = GetCurrentSiteId();
+            badge.AltText = badge.AltText.Trim();
             var result = await _badgeRepository.AddSaveAsync(GetClaimId(ClaimType.UserId), badge);
 
             result.Filename = WriteBadgeFile(result, imageFile);
@@ -68,6 +69,7 @@ namespace GRA.Domain.Service
         public async Task<Badge> UpdateBadgeAsync(Badge badge)
         {
             badge.SiteId = GetCurrentSiteId();
+            badge.AltText = badge.AltText.Trim();
             return await _badgeRepository.AddSaveAsync(GetClaimId(ClaimType.UserId), badge);
         }
 
