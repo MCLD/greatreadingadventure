29cd87c51c10ac973b152d79d821bdbfc3b1b108
madison-boman <madi-boman@mcldaz.org>
2020-04-27 11:10:11 -0700

    Adding initial MC premade avatar view

---
+++ src/GRA.Controllers/ViewModel/MissionControl/Avatar/PremadeAvatarsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GRA.Controllers.ViewModel.MissionControl.Avatar
+{
+    public class PremadeAvatarsViewModel
+    {
+    }
+}
