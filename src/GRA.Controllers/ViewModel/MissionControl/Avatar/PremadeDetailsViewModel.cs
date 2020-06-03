b56b78be93bcec158a4f9c9cf4fcc3ab817e3cb2
madison-boman <madi-boman@mcldaz.org>
2020-06-03 14:11:55 -0700

    Adding updating premade avatar

--- src/GRA.Controllers/ViewModel/MissionControl/Avatar/PremadeDetailsViewModel.cs
+++ src/GRA.Controllers/ViewModel/MissionControl/Avatar/PremadeDetailsViewModel.cs
@@ -8,15 +8,13 @@ namespace GRA.Controllers.ViewModel.MissionControl.Avatar
         public List<List<AvatarLayer>> LayerGroupings { get; set; }
         public ICollection<AvatarBundle> Bundles { get; set; }
         public AvatarBundle Bundle { get; set; }
-        public int DefaultLayer { get; set; }
         public string ImagePath { get; set; }
         public bool NewAvatar { get; set; }
-        public ICollection<AvatarItem> LayerItems { get; set; }
-        public ICollection<AvatarColor> LayerColors { get; set; }
         public List<int> SelectedItemIds { get; set; }
         public ICollection<AvatarItem> SelectedItems { get; set; }
         public string Description { get; set; }
         public string Name { get; set; }
         public int AssociatedBundleId { get; set; }
+        public int BundleId { get; set; }
     }
 }
