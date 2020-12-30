464198eaee16218f353e811c28dc48f53c486e11
Harald Nagel <haraldnagel@mcldaz.org>
2020-12-30 16:57:46 -0700

    Reduce verbosity for incorporating avatars

--- release-prep.bash
+++ release-prep.bash
@@ -6,5 +6,5 @@ set -o pipefail
 echo "=== Downloading and decompressing avatar package"
 curl -Lo defaultavatars.tgz https://github.com/MCLD/gra-avatars/archive/v4.2.0.tar.gz
 mkdir -p src/GRA.Web/assets/defaultavatars
-tar -xvzf defaultavatars.tgz --strip-components=1 -C src/GRA.Web/assets/defaultavatars
+tar -xzf defaultavatars.tgz --strip-components=1 -C src/GRA.Web/assets/defaultavatars
 rm defaultavatars.tgz
