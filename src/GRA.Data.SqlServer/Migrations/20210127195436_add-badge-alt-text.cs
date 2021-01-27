830f09a85aa6cbd3cbf95488c678f2c8e9dc1338
Harald Nagel <haraldnagel@mcldaz.org>
2021-01-27 12:58:06 -0700

    Recreate database migration

---
+++ src/GRA.Data.SqlServer/Migrations/20210127195436_add-badge-alt-text.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GRA.Data.SqlServer.Migrations
+{
+    public partial class addbadgealttext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AltText",
+                table: "Badges",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AltText",
+                table: "Badges");
+        }
+    }
+}
