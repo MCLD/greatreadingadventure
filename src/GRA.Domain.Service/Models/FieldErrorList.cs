1ddcfb429093c1085d6a5dc168d9f9b049717c71
Harald Nagel <haraldnagel@mcldaz.org>
2021-03-08 18:31:53 -0700

    Fix vendor code message, duplicate field error
    - Fix vendor code message to show properly
    - Fix issue if duplicate field validation errors are added

---
+++ src/GRA.Domain.Service/Models/FieldErrorList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GRA.Domain.Service.Models
+{
+    public class FieldErrorList
+    {
+        private readonly IList<KeyValuePair<string, string>> errorList;
+
+        public FieldErrorList()
+        {
+            errorList = new List<KeyValuePair<string, string>>();
+        }
+
+        public void Add(string fieldName, string errorMessage)
+        {
+            errorList.Add(new KeyValuePair<string, string>(fieldName, errorMessage));
+        }
+
+        public ILookup<string, string> AsILookup()
+        {
+            return errorList.ToLookup(_ => _.Key, _ => _.Value);
+        }
+    }
+}
