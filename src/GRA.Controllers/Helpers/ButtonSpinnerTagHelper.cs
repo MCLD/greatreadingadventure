af50b5a167bead30a1d12c6f9567ed618316b5cd
Harald Nagel <haraldnagel@mcldaz.org>
2021-08-31 12:00:37 -0700

    Fix additional FontAwesome and spinner items

--- src/GRA.Controllers/Helpers/ButtonSpinnerTagHelper.cs
+++ src/GRA.Controllers/Helpers/ButtonSpinnerTagHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Text;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
@@ -7,35 +9,41 @@ namespace GRA.Controllers.Helper
     [HtmlTargetElement(targetElement, Attributes = attributeName)]
     public class ButtonSpinnerTagHelper : TagHelper
     {
-        private const string targetElement = "button";
         private const string attributeName = "button-spinner";
-        private const string ignoreValidationAttributeName = "ignore-validation";
         private const string classAttribute = "class";
-        private const string spinnerClass = "btn-spinner";
+        private const string ignoreValidationAttributeName = "ignore-validation";
         private const string ignoreValidationClass = "spinner-ignore-validation";
+        private const string spanClass = "fas fa-spinner fa-pulse fa-fw hidden";
         private const string spanTag = "span";
-        private const string spanClass = "fa fa-spinner fa-pulse fa-fw hidden";
+        private const string spinnerClass = "btn-spinner";
+        private const string targetElement = "button";
 
         [HtmlAttributeName(ignoreValidationAttributeName)]
         public bool IgnoreValidation { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            var existingClasses = output.Attributes.FirstOrDefault(f => f.Name == classAttribute);
-            var buttonClasses = string.Empty;
-            if (existingClasses != null)
+            if (output == null)
             {
-                buttonClasses = existingClasses.Value.ToString();
-                output.Attributes.Remove(existingClasses);
-            }
-            buttonClasses += $" {spinnerClass}";
-            if (IgnoreValidation)
-            {
-                buttonClasses += $" {ignoreValidationClass}";
+                throw new ArgumentNullException(nameof(output));
             }
 
-            var buttonClassAttribute = new TagHelperAttribute(classAttribute, buttonClasses);
-            output.Attributes.Add(buttonClassAttribute);
+            var existingClasses = output.Attributes.FirstOrDefault(f => f.Name == classAttribute);
+            var buttonClasses = new StringBuilder();
+            if (existingClasses != null)
+            {
+                buttonClasses.Append(existingClasses.Value).Append(' ');
+                output.Attributes.Remove(existingClasses);
+            }
+
+            buttonClasses.Append(spinnerClass).Append(' ');
+            if (IgnoreValidation)
+            {
+                buttonClasses.Append(ignoreValidationClass);
+            }
+
+            output.Attributes.Add(new TagHelperAttribute(classAttribute,
+                buttonClasses.ToString().Trim()));
 
             var spinnerSpan = new TagBuilder(spanTag)
             {
