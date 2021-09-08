ae6019b80ae9a006865b6220d85388aa4187232b
madison-boman <maddieboman@gmail.com>
2021-09-08 10:24:29 -0700

    Add functioning page

---
+++ src/GRA.Controllers/ParticipatingBranchesController.cs
@@ -0,0 +1,28 @@
+﻿using GRA.Domain.Service;
+using GRA.Controllers.ViewModel.ParticipatingBranches;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace GRA.Controllers
+{
+    public class ParticipatingBranchesController : Base.UserController
+    {
+        private readonly SiteService _siteService;
+        public ParticipatingBranchesController(ServiceFacade.Controller context,
+            SiteService siteService)
+            : base(context)
+        {
+            _siteService = siteService ?? throw new ArgumentNullException(nameof(siteService));
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = new ParticipatingBranchesViewModel
+            {
+                Systems = await _siteService.GetSystemList()
+            };
+            return View(nameof(Index), viewModel);
+        }
+    }
+}
---
+++ src/GRA.Controllers/ViewModel/ParticipatingBranches/ParticipatingBranchesViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace GRA.Controllers.ViewModel.ParticipatingBranches
+{
+    public class ParticipatingBranchesViewModel
+    {
+        public IEnumerable<Domain.Model.System> Systems { get; set; }
+    }
+}
