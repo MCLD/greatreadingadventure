e8a37124d2883ab48187e24df2cebb7b2faeb738
Harald Nagel <haraldnagel@mcldaz.org>
2022-01-31 17:05:19 -0700

    Add migration

---
+++ src/GRA.Data.SqlServer/Migrations/20220201000319_challenge_popularity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GRA.Data.SqlServer.Migrations
+{
+    public partial class challenge_popularity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EstimatedPopularity",
+                table: "Challenges",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EstimatedPopularity",
+                table: "Challenges");
+        }
+    }
+}
