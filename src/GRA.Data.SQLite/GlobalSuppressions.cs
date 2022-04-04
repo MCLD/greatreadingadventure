09dfedf3d5a04011a248f5bb91be392d7ec468e3
Harald Nagel <haraldnagel@mcldaz.org>
2022-04-04 15:28:11 -0700

    Suppress quality notices for generated SQLite code

---
+++ src/GRA.Data.SQLite/GlobalSuppressions.cs
@@ -0,0 +1,27 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design",
+    "CA1062:Validate arguments of public methods",
+    Justification = "Generated EF Code",
+    Scope = "namespaceanddescendants",
+    Target = "~N:GRA.Data.SQLite.Migrations")]
+[assembly: SuppressMessage("Design",
+    "CA1505:Avoid unmaintainable code",
+    Justification = "Generated EF Code",
+    Scope = "namespaceanddescendants",
+    Target = "~N:GRA.Data.SQLite.Migrations")]
+[assembly: SuppressMessage("Naming",
+    "CA1707:Identifiers should not contain underscores",
+    Justification = "Generated EF Code",
+    Scope = "namespaceanddescendants",
+    Target = "~N:GRA.Data.SQLite.Migrations")]
+[assembly: SuppressMessage("Style",
+    "IDE1006:Naming Styles",
+    Justification = "Generated EF Code",
+    Scope = "namespaceanddescendants",
+    Target = "~N:GRA.Data.SQLite.Migrations")]
