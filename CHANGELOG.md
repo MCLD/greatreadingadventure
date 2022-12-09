9d1f3a0f0b9bd88716f0eb269409f6f7e70b260c
Raymond Rollinger <PaulRollinger@mcldaz.org>
2022-12-09 11:08:09 -0700

    Clean & reorder affected files for prize redemption instructions and notes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,14 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/).
 
 ## Unreleased
 
+## Changed
+
+- Rename "Redemption Instructions" label to "Redemptions Instructions for Staff"
+
+## Added
+
+- Notes field on prize redemption for staff
+
 ### Fixed
 
 - Display a friendly and accurate error if a social image is too large
--- src/GRA.Data/Model/PrizeWinner.cs
+++ src/GRA.Data/Model/PrizeWinner.cs
@@ -5,25 +5,31 @@ namespace GRA.Data.Model
 {
     public class PrizeWinner : Abstract.BaseDbEntity
     {
+        public Drawing Drawing { get; set; }
+
+        public int? DrawingId { get; set; }
+
+        public int? MailId { get; set; }
+
+        public DateTime? RedeemedAt { get; set; }
+
+        public int? RedeemedBy { get; set; }
+
+        public int? RedeemedByBranch { get; set; }
+
+        public int? RedeemedBySystem { get; set; }
+
         [Required]
         public int SiteId { get; set; }
-        public int? DrawingId { get; set; }
-        public Drawing Drawing { get; set; }
-        public int? TriggerId { get; set; }
+        [MaxLength(255)]
+        public string StaffNotes { get; set; }
+
         public Trigger Trigger { get; set; }
+        public int? TriggerId { get; set; }
+        public User User { get; set; }
 
         [Required]
         public int UserId { get; set; }
-
-        public User User { get; set; }
-
-        public DateTime? RedeemedAt { get; set; }
-        public int? RedeemedBy { get; set; }
-        public int? RedeemedByBranch { get; set; }
-        public int? RedeemedBySystem { get; set; }
-        public int? MailId { get; set; }
         public int? VendorCodeId { get; set; }
-        [MaxLength(255)]
-        public string StaffNotes { get; set; }
     }
 }
