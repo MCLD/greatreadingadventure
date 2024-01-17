a67cdee550e4984b9aafa81858ddd5c06fac1540
Harald Nagel <dev@hpn.is>
2024-01-17 10:00:04 -0700

    Add migration

---
+++ src/GRA.Data.SqlServer/Migrations/20240116234954_add-books-profile-history.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GRA.Data.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class addbooksprofilehistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BookId",
+                table: "UserLogs",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookId",
+                table: "UserLogs");
+        }
+    }
+}
