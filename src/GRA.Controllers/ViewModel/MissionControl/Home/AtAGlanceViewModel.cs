8b9be45364a2b06336db7772cf0fd9c1eb09c0b1
Harald Nagel <dev@hpn.is>
2024-09-13 16:56:56 -0700

    Improve Mission Control dashboard view

--- src/GRA.Controllers/ViewModel/MissionControl/Home/AtAGlanceViewModel.cs
+++ src/GRA.Controllers/ViewModel/MissionControl/Home/AtAGlanceViewModel.cs
@@ -6,14 +6,15 @@ namespace GRA.Controllers.ViewModel.MissionControl.Home
 {
     public class AtAGlanceViewModel
     {
+        public AtAGlanceReport AtAGlanceReport { get; set; }
+        public int? Category { get; set; }
+        public bool IsNewsSubscribed { get; set; }
+        public int LatestNewsId { get; set; }
+        public IEnumerable<NewsCategory> NewsCategories { get; set; }
         public IEnumerable<NewsPost> NewsPosts { get; set; }
         public PaginateViewModel PaginateModel { get; set; }
-        public IEnumerable<NewsCategory> NewsCategories { get; set; }
-        public List<bool> WithinAWeek { get; set; }
-        public AtAGlanceReport AtAGlanceReport { get; set; }
         public bool ShowPosts { get; set; }
-        public bool IsNewsSubscribed { get; set; }
         public string SiteAdministratorEmail { get; set; }
-        public int LatestNewsId { get; set; }
+        public List<bool> WithinAWeek { get; set; }
     }
 }
