108d5f8b6dbd7104d2e18b99c0bda7d97b448042
Adam Abrams-Flohr <AdamAbrams-Flohr@mcldaz.org>
2025-06-26 15:30:21 -0700

    Expand TipDetailViewModel to connect with frontend

--- src/GRA.Controllers/ViewModel/MissionControl/DailyTips/TipDetailViewModel.cs
+++ src/GRA.Controllers/ViewModel/MissionControl/DailyTips/TipDetailViewModel.cs
@@ -7,5 +7,7 @@ namespace GRA.Controllers.ViewModel.MissionControl.DailyTips
     public class TipDetailViewModel
     {
         public PaginateViewModel PaginateModel { get; set; }
+        public ICollection<DailyLiteracyTipImage> Images { get; set; }
+        public DailyLiteracyTip Tip { get; set; }
     }
 }
