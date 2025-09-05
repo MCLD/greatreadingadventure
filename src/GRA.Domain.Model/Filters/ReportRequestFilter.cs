38069ed42b2f743163835a1cc8748597a200f928
Adam Abrams-Flohr <AdamAbrams-Flohr@mcldaz.org>
2025-09-05 12:43:18 -0700

    Create model layer for new display page for generated reports

---
+++ src/GRA.Domain.Model/Filters/ReportRequestFilter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GRA.Domain.Model.Filters
+{
+    public class ReportRequestFilter : BaseFilter
+    {
+        public int? ReportId { get; set; }
+        public int? RequestedByUserId { get; set; }
+    }
+}
