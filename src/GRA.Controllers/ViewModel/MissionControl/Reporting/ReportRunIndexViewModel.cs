544da7acfd15af8a97c32402e4d2d637858920cb
Adam Abrams-Flohr <AdamAbrams-Flohr@mcldaz.org>
2025-09-08 13:35:00 -0700

    Create basic view without pagination for the report history

---
+++ src/GRA.Controllers/ViewModel/MissionControl/Reporting/ReportRunIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GRA.Controllers.ViewModel.Shared;
+using GRA.Domain.Model;
+using GRA.Domain.Model.Filters;
+
+namespace GRA.Controllers.ViewModel.MissionControl.Reporting
+{
+    public class ReportRunIndexViewModel
+    {
+        public ICollection<ReportRunSummary> Runs { get; set; }
+        public PaginateViewModel Pagination { get; set; }
+        public ReportRequestFilter Filter { get; set; }
+    }
+}
