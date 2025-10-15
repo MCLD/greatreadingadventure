b74a44b9f088913f200beb91f7c26a365f77f189
Dan Wilcox <DanielWilcox@mcldaz.org>
2025-10-15 10:25:26 -0700

    Add BranchId token to vendor code URLs

--- src/GRA/TemplateToken.cs
+++ src/GRA/TemplateToken.cs
@@ -2,6 +2,7 @@
 {
     public readonly struct TemplateToken : System.IEquatable<TemplateToken>
     {
+        public static readonly string VendorBranchToken = "BranchId";
         public static readonly string VendorCodeToken = "Code";
         public static readonly string VendorLinkToken = "Link";
 
