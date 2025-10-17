bce205294bb439014389f20705f251401811e643
Dan Wilcox <DanielWilcox@mcldaz.org>
2025-10-17 15:37:05 -0700

    Add join codes

---
+++ src/GRA.Data/Model/JoinCode.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GRA.Data.Model
+{
+    public class JoinCode : Abstract.BaseDbEntity
+    {
+        public int SiteId { get; set; }
+
+        [Required]
+        [MaxLength(8)]
+        public string Code { get; set; }
+
+        public bool IsQRCode { get; set; }
+
+        public int? BranchId { get; set; }
+        public Branch Branch { get; set; }
+
+        public int AccessCount { get; set; }
+        public int JoinCount { get; set; }
+        
+    }
+}
