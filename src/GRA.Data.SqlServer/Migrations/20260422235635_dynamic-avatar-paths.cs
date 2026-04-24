d7d371140b86db97cf70a01cebb7629e32d99562
Harald Nagel <dev@hpn.is>
2026-04-24 16:53:21 -0700

    Add avatar database migration

---
+++ src/GRA.Data.SqlServer/Migrations/20260422235635_dynamic-avatar-paths.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GRA.Data.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class dynamicavatarpaths : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Icon",
+                table: "AvatarLayers");
+
+            migrationBuilder.DropColumn(
+                name: "Thumbnail",
+                table: "AvatarItems");
+
+            migrationBuilder.DropColumn(
+                name: "Filename",
+                table: "AvatarElements");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Icon",
+                table: "AvatarLayers",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Thumbnail",
+                table: "AvatarItems",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Filename",
+                table: "AvatarElements",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
